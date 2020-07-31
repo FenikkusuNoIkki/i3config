155a19609b2e36a7a57f999115520ff1629356f2
François Détré <francois.detre@gmail.com>
2020-07-31 17:31:06 +0200

    Update memory script

--- i3blocks/memory.sh
+++ i3blocks/memory.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 memory=$(free -m | awk 'NR==2{printf "%s/%s MB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-echo "$memory"
+swap=$(free -m | awk 'NR==3{printf "%s/%s MB (%.2f%%)\n", $3,$2,$3*100/$2 }')
+memory_percent=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2 }')
+swap_percent=$(free -m | awk 'NR==3{printf "%.2f\n", $3*100/$2 }')
+# First echo updates the full_text i3bar key
+echo "$memory - $swap"
+# Second echo updates the short_text i3bar key
+echo "$memory - $swap"
+if [ $(echo "$swap_percent>0"| bc) -eq 1 ] || [ $(echo "$memory_percent>=90"| bc) -eq 1 ]
+then
+    # Third echo updates the color i3bar key
+    echo "#af3a03"
+elif [ $(echo "$memory_percent>=70"| bc) -eq 1 ] && [ $(echo "$memory_percent<90"| bc) -eq 1 ]
+then
+    # Third echo updates the color i3bar key
+    echo "#b57614"
+fi
+
