d143a0864203ea16d1c91cd2fdc697f47d64bd7e
François Détré <francois.detre@gmail.com>
2020-08-01 23:46:12 +0200

    Update ethernet script

--- i3blocks/ethernet.sh
+++ i3blocks/ethernet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ifname=$(ip link | awk -F: '$0 !~ "lo|vir|^[^0-9]"{print $2;getline}')
+ifname=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
 ip=$(ip addr show $ifname | awk '$1 ~ /^inet$/ {printf "%s\n", $2}' | sed 's/\/.*//')
 echo "$ip"
