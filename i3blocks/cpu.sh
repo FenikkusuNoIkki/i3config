f06320f820606bc49fd2326be2e65551e248789d
François Détré <francois.detre@gmail.com>
2020-08-01 20:38:44 +0200

    Update cpu script

--- i3blocks/cpu.sh
+++ i3blocks/cpu.sh
@@ -10,10 +10,6 @@ cpu_idle=$((cpu_now[4]))
 # Calc percentage 
 cpu_usage=$((100-(100 * cpu_idle / cpu_sum)))
 
-# Keep this as last for our next read 
-cpu_last=("${cpu_now[@]}") 
-cpu_last_sum=$cpu_sum 
-
 # First echo updates the full_text i3bar key
 echo "$cpu_usage%" 
 # Second echo updates the short_text i3bar key
