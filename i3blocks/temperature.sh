bbddd08a21282dd04a21ca9fbd0dd6ac8c9a1a7b
François Détré <francois.detre@gmail.com>
2020-08-01 12:00:02 +0200

    Add temp colors

--- i3blocks/temperature.sh
+++ i3blocks/temperature.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo $(sensors | grep -F 'Core' | awk '{print $3}' | tr -s '\n' ' ' | sed 's/+//g')
+temp=$(sensors | grep -F 'Core' | awk '{print $3}' | tr -s '\n' ' ' | sed 's/+//g')
+# First echo updates the full_text i3bar key
+echo "$temp"
+# Second echo updates the short_text i3bar key
+echo "$temp"
+for test in $temp
+do
+    core_temp="${test//[^0-9.]/}"
+    if [ $(echo "$core_temp>=60"| bc) -eq 1 ] && [ $(echo "$core_temp<90"| bc) -eq 1 ]
+    then
+	# Third echo updates the color i3bar key
+	echo "#b57614"
+	break
+    elif [ $(echo "$core_temp>=90"| bc) -eq 1 ] 
+	# Third echo updates the color i3bar key
+	echo "#af3a03"
+    fi
+done
