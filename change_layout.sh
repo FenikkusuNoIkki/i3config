e05fcac3f65b9a6effbf26ad0ea1b2584e9a73e3
François Détré <francois.detre@gmail.com>
2020-08-06 08:23:26 +0200

    Imp automatic switch language conf

--- change_layout.sh
+++ change_layout.sh
@@ -3,6 +3,9 @@ keyboard_language=$(setxkbmap -query | awk ' /layout/ {print $2}')
 if [ $keyboard_language == "fr" ] || [ $keyboard_language == "fr-latin9" ]
 then
     setxkbmap us
+    ln -sf ~/.config/i3/config_en ~/.config/i3/config
 else
     setxkbmap fr-latin9
+    ln -sf ~/.config/i3/config_fr ~/.config/i3/config
 fi
+i3-msg reload
