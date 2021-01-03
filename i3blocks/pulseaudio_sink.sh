b084e2cd742ded959b5710baf2215c4c5735605c
Francois DETRE <francois.detre@gmail.com>
2021-01-03 16:09:48 +0100

    Pulseaudio change volume

--- i3blocks/pulseaudio_sink.sh
+++ i3blocks/pulseaudio_sink.sh
@@ -13,6 +13,72 @@ get_available_sinks_list () {
 	done
 }
 
+## Increase/Decrease the volume for the selected sink
+## $1 increase or decrease
+increase_decrease_volume_sink () {
+	pacmd_list_sinks=$(pacmd list-sinks)
+	pacmd_list_sink_inputs=$(pacmd list-sink-inputs)
+	nb_inputs_sink=$(echo "$pacmd_list_sink_inputs" | head -n1 | awk '{print $1;}')
+	# If no sink in use there's nothing to do
+	if [ $((nb_inputs_sink)) == 0 ]; then
+		exit 1
+	fi
+
+	# Register the sink description for the current apps with its index
+	declare -A sink_index
+	for (( x=1; x <= "$((nb_inputs_sink))"; x++ )); do
+		sink=$(echo "$pacmd_list_sink_inputs" | awk '/sink:/{i++} i=='$x'{print $2; exit}')
+		sink_description=$(echo "$pacmd_list_sinks" | sed -e "1,/index: $(($sink))/ d" | awk '/device.description =/{for (x=3; x<=NF; x++) printf("%s ",$x); exit}' | sed 's/"//g' | sed 's/.$//')
+		sink_index[$sink_description]="$sink"
+	done
+
+	# Register the current sinks for the rofi menu
+	count=1
+	for key in "${!sink_index[@]}"; do
+		echo "$key - ${sink_index[$key]}"
+		rofi_current_sinks+="$key"
+		if [ $count != ${#sink_index[@]} ]; then
+			rofi_current_sinks+="\n"
+		fi
+		((count++))
+	done
+
+	# Display the rofi menu with the sinks / If only one sink no need to display the menu selection
+	if [ "${#sink_index[@]}" -gt "1" ]; then
+		if [ "$1" == "inc" ]; then
+			chosen_sink="$(echo -e "$rofi_current_sinks" | rofi -dmenu -p "[Increase volume] Select the sink" -lines ${#sink_index[@]})"
+		elif [ "$1" == "dec" ]; then
+			chosen_sink="$(echo -e "$rofi_current_sinks" | rofi -dmenu -p "[Decrease volume] Select the sink" -lines ${#sink_index[@]})"
+		fi
+		# If cancel (escape key) nothing to do
+		if [ -z "$chosen_sink" ]; then
+			exit 1
+		fi
+	fi
+
+	# Register the sink index and the volume for the chosen sink
+	for key in "${!sink_index[@]}"; do
+		# If only one app save the informations on the first occurence and exit the loop
+		if [ ${#sink_index[@]} == 1 ]; then
+			index_to_change="$key"
+			current_volume=$(echo "$pacmd_list_sinks" | sed -e "1,/index: ${sink_index[$key]}/ d" | awk '/volume:/{print $5; exit}' | sed 's/"//g' | sed 's/%//g')
+			break
+		fi
+		if [ "$key" == "$chosen_sink" ]; then
+			index_to_change="${sink_index[$key]}"
+			current_volume=$(echo "$pacmd_list_sinks" | sed -e "1,/index: ${sink_index[$key]}/ d" | awk '/volume:/{print $5; exit}' | sed 's/"//g' | sed 's/%//g')
+			break
+		fi
+	done
+	echo "$index_to_change - $current_volume"
+	if [ "$1" == "inc" ]; then
+		new_vol_level=$(((current_volume + 10)*65536/100))
+	elif [ "$1" == "dec" ]; then
+		new_vol_level=$(((current_volume - 10)*65536/100))
+	fi
+	pacmd set-sink-volume "$index_to_change" "$new_vol_level"
+}
+
 ## Change the audio source for the application selected in the rofi menu
 change_sinks () {
 	pacmd_list_sink_inputs=$(pacmd list-sink-inputs)
@@ -64,6 +130,7 @@ change_sinks () {
 		if [ "${application_list[$key]}" == "$chosen_app" ]; then
 			echo "${application_list[$key]} - $key"
 			index_to_change="$key"
+			break
 		fi
 	done
 
@@ -88,6 +155,7 @@ change_sinks () {
 		if [ "${available_sinks_list[$key]}" == "$chosen_sink" ]; then
 			echo "${available_sinks_list[$key]} - $key"
 			new_index="$key"
+			break
 		fi
 	done
 
@@ -137,13 +205,19 @@ display_sinks () {
 	fi
 }
 
-while getopts "ds" option; do
+while getopts "gsid" option; do
 	case "${option}" in
-		d) # display the sink informations in the i3blocks bar
+		g) # display the sink informations in the i3blocks bar
 			display_sinks
 			;;
 		s) # change the sink for the specified application
 			change_sinks
+			;;
+		i) # Increase the volume for the specified sink
+			increase_decrease_volume_sink inc
+			;;
+		d) # Decrease the volume for the specified sink
+			increase_decrease_volume_sink dec
 			;;
 		*) # incorrect option
             echo "Error: Invalid option $option"
